d6fd591dc794fda3942fb1a7851aacb00893ed39
Brian Muñoz <munoz4music@gmail.com>
2025-10-17 21:35:07 -0400

    Website design overhaul

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
